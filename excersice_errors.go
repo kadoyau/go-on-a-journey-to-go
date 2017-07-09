626c6f8c4da0c5e81ef793b08080d1e60c3f317d
kadoyau <el.psi.congroo@gmail.com>
2017-07-09 22:22:36 +0900

    errorsの演習問題を解いた

---
+++ excersice_errors.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %v", float64(e))
+}
+
+func Sqrt(x float64) (float64, error) {
+	z := 1.0
+	var old float64
+
+	for i := 0; (old-z)*(old-z) > 0.00000001; i++ {
+		old = z
+		z = z - (z*z-x)/2*z
+	}
+
+	if x < 0 {
+		return z, ErrNegativeSqrt(x)
+	} else {
+		return z, nil
+	}
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(-2))
+}
